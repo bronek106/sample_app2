41853479a83d14a273f28d044240034d7644ba9b
Bronek <bronek106@gmail.com>
2014-01-20 13:28:09 +0100

    Default index page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleApp2::Application.routes.draw do
+  root 'hello#index'
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
